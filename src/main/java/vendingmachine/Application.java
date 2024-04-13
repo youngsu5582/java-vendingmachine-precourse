1606337caae16ab5396e361aca590f8e3044ce5a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-13 14:37:40 +0900

    feat(Application): 기능 구현 초안 완료
    - README 에도 체크

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -2,6 +2,27 @@ package vendingmachine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Money coinMoney = Money.from(InputView.inputCoinMoney());
+            final CoinBoard coinBoard = new CoinBoard(new CoinRandomGenerator().generate(coinMoney));
+            OutputView.printCoinBoard(coinBoard.toMap());
+            final Products products = Products.ofInfo(InputView.inputProductsInfo());
+
+            final VendingMachine vendingMachine = new VendingMachine(coinBoard, products);
+
+            Money purchaseMoney = Money.from(InputView.inputPurchaseMoney());
+
+            while (vendingMachine.canPurchase(purchaseMoney)) {
+                OutputView.printMoney(purchaseMoney.value());
+                final String productName = InputView.inputPurchaseProductName();
+                purchaseMoney = vendingMachine.purchase(purchaseMoney, productName);
+            }
+            OutputView.printMoney(purchaseMoney.value());
+            OutputView.printChange(vendingMachine.change(purchaseMoney));
+
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+        }
+
     }
 }
